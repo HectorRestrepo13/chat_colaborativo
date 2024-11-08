a6fadd0f5a0f1f30aaef3db6990a3e3853cb1f4f
Miguel-Alzate <miguelangel4817@gmail.com>
2024-11-08 15:17:31 -0500

    fix: correccion de sintaxis

--- docs/bd/bd.sql
+++ docs/bd/bd.sql
@@ -10,7 +10,7 @@ USE DBChatColaborativo;
 -- Tabla roles para determinar el rol del usuario dentro del sistema.
 CREATE TABLE roles (
     id_rol INT PRIMARY KEY AUTO_INCREMENT, 
-    nombre_rol VARCHAR(20) NOT NULL UNIQUE, 
+    nombre_rol VARCHAR(20) NOT NULL UNIQUE
 );
 
 -- Tabla usuarios con los datos requeridos.
@@ -22,8 +22,9 @@ CREATE TABLE usuarios (
     foto_perfil VARCHAR(255),
     clave VARCHAR(255) NOT NULL, 
     estado_usuario BIT NOT NULL DEFAULT 1,  -- Estado del usuario (activo = 1, inactivo = 0).
+    enviar_mensajes BIT NOT NULL DEFAULT 0,  -- Estado para saber si el usuario puede mandar mensajes al grupo (puede = 1, no puede = 0).
+    contacto_directo BIT NOT NULL DEFAULT 0, -- Contacto directo (se le pueden enviar mensajes a este usuario = 1, no = 0).
     fecha_registro DATETIME,
-    contacto_directo BIT NOT NULL DEFAULT 0 -- Contacto directo (se le pueden enviar mensajes a este usuario = 1, no = 0).
     id_rol INT NOT NULL, 
     FOREIGN KEY (id_rol) REFERENCES roles(id_rol)
 );
@@ -47,8 +48,8 @@ CREATE TABLE mensajes (
     tipo_contenido VARCHAR(45) NOT NULL,
     fecha_envio DATETIME NOT NULL,
     multimedia VARCHAR(255),
-    estado_lectura BIT DEFAULT 0; -- 1 para leido, 0 para no leido
-    estado_mensaje BIT DEFAULT 1; -- 1 para activo, 0 para eliminado
+    estado_lectura BIT DEFAULT 0, -- 1 para leido, 0 para no leido
+    estado_mensaje BIT DEFAULT 1, -- 1 para activo, 0 para eliminado
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
 
@@ -76,7 +77,7 @@ CREATE TABLE mensajes_directo (
 CREATE TABLE integrantes_grupo (
     id_grupo INT,
     id_usuario INT,
-    fecha_ingreso DATETIME NOT NULL
+    fecha_ingreso DATETIME NOT NULL,
     PRIMARY KEY (id_grupo, id_usuario),
     FOREIGN KEY (id_grupo) REFERENCES grupos(id_grupo),
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
